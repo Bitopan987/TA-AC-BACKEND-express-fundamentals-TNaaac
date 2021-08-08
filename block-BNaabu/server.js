b9223f4481eedb0de19dd6dd7f2a42a2d88ec667
bitopan987 <bitopanxon1@gmail.com>
2021-08-08 23:20:02 +0530

    refactor

--- block-BNaabu/server.js
+++ block-BNaabu/server.js
@@ -17,7 +17,7 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  res.send(err);
+  res.status(500).send(err);
 });
 app.listen(4000, () => {
   console.log('Listening to port 4k');
